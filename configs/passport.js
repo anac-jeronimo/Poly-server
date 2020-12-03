a87ab8552e93c7cffe90436c03e33b0f9c4c8050
anarita_cavalheiro <anarita.cavalheiro@hotmail.com>
2020-12-03 14:32:19 +0000

    api update, will crash

--- configs/passport.js
+++ configs/passport.js
@@ -1,7 +1,7 @@
 const passport = require("passport");
 const LocalStrategy = require("passport-local");
 const bcrypt = require("bcryptjs");
-const User = require("../models/User-model");
+const User = require("../models/user-model");
 const GoogleStrategy = require("passport-google-oauth20").Strategy;
 //this are passport mathods
 //we know what they do but not how they work
