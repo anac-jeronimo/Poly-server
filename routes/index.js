e1cd3605f2ea55f508242c435280d51781833502
anarita_cavalheiro <anarita.cavalheiro@hotmail.com>
2020-12-03 15:10:25 +0000

    cloudinary and api done

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,14 @@
-const express = require('express');
-const router  = express.Router();
-
+const express = require("express");
+const router = express.Router();
+const vision = require("@google-cloud/vision");
+const getColor = require("../utils/api");
 /* GET home page */
-router.get('/', (req, res, next) => {
-  res.render('index');
+router.get("/", (req, res, next) => {
+  res.render("index");
+});
+
+router.get("/getColor", (req, res) => {
+  getColor("public/images/2020-08-08 (2).png");
 });
 
 module.exports = router;
--- utils/api.js
+++ utils/api.js
@@ -1,11 +1,11 @@
 const vision = require("@google-cloud/vision");
-const client = new vision.ImageAnnotatorClient();
 
-const [result] = await client.imageProperties(
-  "public/images/2020-08-08 (2).png"
-);
-const colors = result.imagePropertiesAnnotation.dominantColors.colors;
-colors.forEach((color) => console.log(color));
-const [result] = await client.imageProperties(
-  `gs://${public}/${public / images / 2020 - 08 - 08(2).png}`
-);
+const getColor = async (img) => {
+  const client = new vision.ImageAnnotatorClient();
+
+  const [result] = await client.imageProperties(img);
+  const colors = result.imagePropertiesAnnotation.dominantColors.colors;
+  colors.forEach((color) => console.log(color));
+};
+
+module.exports = getColor;
