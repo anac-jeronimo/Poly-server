c253b971af9e7690e8c6b9f7b7f3fc2fe5646579
anarita_cavalheiro <anarita.cavalheiro@hotmail.com>
2020-12-04 10:28:30 +0000

    git completed

--- bin/seeds.js
+++ bin/seeds.js
@@ -1,98 +1,118 @@
-require('dotenv').config();
-const mongoose = require('mongoose');
-const Color = require('../models/Color');
-const DB_NAME = 'poly-server';
+require("dotenv").config();
+const mongoose = require("mongoose");
+const Color = require("../models/Color");
+const DB_NAME = "poly-server";
 
-mongoose.connect('mongodb://127.0.0.1:27017/poly-server', {
+mongoose.connect("mongodb://127.0.0.1:27017/poly-server", {
   useCreateIndex: true,
   useNewUrlParser: true,
-  useUnifiedTopology: true
+  useUnifiedTopology: true,
 });
 
 const color = [
   {
     name: "black",
-    imageURL: " "
-  }, 
-  {
-    name: "blue" ,
-    imageURL: 
+    imageURL:
+      "https://res.cloudinary.com/dkadtxtto/image/upload/v1607075911/poly-server/black_o6ztfq.png ",
   },
   {
-    name: "cyan" ,
-    imageURL: 
+    name: "blue",
+    imageURL:
+      "https://res.cloudinary.com/dkadtxtto/image/upload/v1607075460/poly-server/blue_utkgbo.png",
   },
   {
-    name: "green" ,
-    imageURL: 
+    name: "cyan",
+    imageURL:
+      "https://res.cloudinary.com/dkadtxtto/image/upload/v1607075461/poly-server/light-blue_e6dahc.png",
   },
   {
-    name: "teal" ,
-    imageURL: 
+    name: "green",
+    imageURL:
+      "https://res.cloudinary.com/dkadtxtto/image/upload/v1607075462/poly-server/green_zxoroc.png",
   },
   {
-    name: "turquoise" ,
-    imageURL: 
+    name: "teal",
+    imageURL:
+      "https://res.cloudinary.com/dkadtxtto/image/upload/v1607075461/poly-server/dark-green_liu676.png",
   },
   {
-    name: "indigo" ,
-    imageURL: 
+    name: "turquoise",
+    imageURL:
+      "https://res.cloudinary.com/dkadtxtto/image/upload/v1607075461/poly-server/light-blue_e6dahc.png",
   },
   {
-    name: "gray" ,
-    imageURL: 
+    name: "indigo",
+    imageURL:
+      "https://res.cloudinary.com/dkadtxtto/image/upload/v1607075462/poly-server/violet_z6dsiu.png",
   },
   {
-    name: "purple" ,
-    imageURL: 
+    name: "gray",
+    imageURL:
+      "https://res.cloudinary.com/dkadtxtto/image/upload/v1607075461/poly-server/dark-grey_xsimau.png",
   },
   {
-    name: "brown" ,
-    imageURL: 
+    name: "purple",
+    imageURL:
+      "https://res.cloudinary.com/dkadtxtto/image/upload/v1607075462/poly-server/purple_ktslw0.png",
   },
   {
-    name: "tan" ,
-    imageURL: 
+    name: "brown",
+    imageURL:
+      "https://res.cloudinary.com/dkadtxtto/image/upload/v1607075461/poly-server/brown_eyhasd.png",
   },
   {
-    name: "violet" ,
-    imageURL: 
+    name: "tan",
+    imageURL:
+      "https://res.cloudinary.com/dkadtxtto/image/upload/v1607075461/poly-server/brown_eyhasd.png",
   },
   {
-    name: "beige" ,
-    imageURL: 
+    name: "violet",
+    imageURL:
+      "https://res.cloudinary.com/dkadtxtto/image/upload/v1607075462/poly-server/violet_z6dsiu.png",
   },
   {
-    name: "fuchsia" ,
-    imageURL: 
+    name: "beige",
+    imageURL:
+      "https://res.cloudinary.com/dkadtxtto/image/upload/v1607075461/poly-server/khaki_swnqzy.png",
   },
   {
-    name: "gold" ,
-    imageURL: 
+    name: "fuchsia",
+    imageURL:
+      "https://res.cloudinary.com/dkadtxtto/image/upload/v1607075462/poly-server/pink_gqc2gw.png",
   },
   {
-    name: "mangenta" ,
-    imageURL: 
+    name: "gold",
+    imageURL:
+      "https://res.cloudinary.com/dkadtxtto/image/upload/v1607075461/poly-server/gold_wzfszp.png",
   },
   {
-    name: "orange" ,
-    imageURL: 
+    name: "mangenta",
+    imageURL:
+      "https://res.cloudinary.com/dkadtxtto/image/upload/v1607075462/poly-server/orchid_zj8vb8.png",
   },
   {
-    name: "pink" ,
-    imageURL: 
+    name: "orange",
+    imageURL:
+      "https://res.cloudinary.com/dkadtxtto/image/upload/v1607075462/poly-server/orange_nysgr5.png",
   },
   {
-    name: "red" ,
-    imageURL: 
+    name: "pink",
+    imageURL:
+      "https://res.cloudinary.com/dkadtxtto/image/upload/v1607075462/poly-server/pink_gqc2gw.png",
   },
   {
-    name: "white" ,
-    imageURL: 
+    name: "red",
+    imageURL:
+      "https://res.cloudinary.com/dkadtxtto/image/upload/v1607075462/poly-server/red_za9qup.png",
   },
   {
-    name: "yellow" ,
-    imageURL: 
-  }
-   
-]
+    name: "white",
+    imageURL:
+      "https://res.cloudinary.com/dkadtxtto/image/upload/v1607075926/poly-server/white_ggxvls.png",
+  },
+  {
+    name: "yellow",
+    imageURL:
+      "https://res.cloudinary.com/dkadtxtto/image/upload/v1607075462/poly-server/yellow_oclecd.png",
+  },
+];
