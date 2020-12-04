27605a4e0a4cc0500edaa428e5d2726c73442bd8
anarita_cavalheiro <anarita.cavalheiro@hotmail.com>
2020-12-04 12:17:47 +0000

    added img to response

--- routes/index.js
+++ routes/index.js
@@ -2,14 +2,17 @@ const express = require("express");
 const router = express.Router();
 const vision = require("@google-cloud/vision");
 const getColor = require("../utils/api");
+const images = require("../bin/images");
+
 /* GET home page */
 router.get("/", (req, res, next) => {
   res.render("index");
 });
 
-router.get("/getColor", (req, res) => {
+router.get("/getcolor", (req, res) => {
   getColor("public/images/2020-08-08 (2).png").then((response) => {
-    res.json(response);
+    let colorName = response;
+    res.json({ imageUrl: images[response], colorName });
   });
 });
 
