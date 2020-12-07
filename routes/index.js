6604714ff24b2aa26c09b925c1aff7cc56d157fe
anarita_cavalheiro <anarita.cavalheiro@hotmail.com>
2020-12-07 18:36:32 +0000

    commited changes

--- routes/index.js
+++ routes/index.js
@@ -1,19 +1,9 @@
 const express = require("express");
 const router = express.Router();
-const vision = require("@google-cloud/vision");
-const getColor = require("../utils/api");
-const images = require("../bin/images");
 
 /* GET home page */
 router.get("/", (req, res, next) => {
   res.render("index");
-});
-
-router.get("/getcolor", (req, res) => {
-  getColor("public/images/2020-08-08 (2).png").then((response) => {
-    let colorName = response;
-    res.json({ imageUrl: images[response], colorName });
-  });
 });
 
 module.exports = router;
--- routes/scan-route.js
+++ routes/scan-route.js
@@ -2,11 +2,23 @@ const express = require("express");
 const { model } = require("mongoose");
 const router = express.Router();
 const fileUpload = require("../configs/cloudinary");
+const vision = require("@google-cloud/vision");
+const getColor = require("../utils/api");
+const images = require("../bin/images");
 
 //upload to cloudinary
 router.post("/upload", fileUpload.single("file"), (req, res) => {
   console.log("in upload");
   res.json({ fileUrl: req.file.path });
+});
+
+router.get("/getcolor/:image", (req, res) => {
+  getColor(
+    `https://res.cloudinary.com/dkadtxtto/image/upload/v1607365566/poly-client/${image}`
+  ).then((response) => {
+    let colorName = response;
+    res.json({ imageUrl: images[response], colorName });
+  });
 });
 
 module.exports = router;
