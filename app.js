6b0e143eecc7669dbb9477b446c8a0fbedb7c4ff
anc-jeronimo <anac_jeronimo@hotmail.com>
2020-12-09 15:16:55 +0000

    fxed log in

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 require("dotenv").config();
-const request = require("request");
+//const request = require("request");
 const bodyParser = require("body-parser");
 const cookieParser = require("cookie-parser");
 const express = require("express");
@@ -17,7 +17,7 @@ const vision = require("@google-cloud/vision");
 require("./configs/passport");
 
 mongoose
-  .connect("mongodb://localhost/poly-server", { useNewUrlParser: true })
+  .connect(process.env.MONGODB_URI, { useNewUrlParser: true })
   .then((x) => {
     console.log(
       `Connected to Mongo! Database name: "${x.connections[0].name}"`
@@ -41,14 +41,6 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 
 // Express View engine setup
-
-app.use(
-  require("node-sass-middleware")({
-    src: path.join(__dirname, "public"),
-    dest: path.join(__dirname, "public"),
-    sourceMap: true,
-  })
-);
 
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "hbs");
